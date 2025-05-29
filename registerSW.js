c657341655473183d5851d6cd0151ccfc33b040f
Aaron <aaron.t.y.lau@gmail.com>
2025-05-29 17:50:48 -0600

    Page

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/GOLU-Page/sw.js', { scope: '/GOLU-Page/' })})}
